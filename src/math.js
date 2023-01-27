c165ec8fa8cb2dc69d6ad52b99f40bca21b4056d
CamposJayme <108497638+CamposJayme@users.noreply.github.com>
2023-01-26 22:30:19 -0300

    function for calculate input datas

--- src/math.js
+++ src/math.js
@@ -1,1 +1,11 @@
-window.confirm('Você acessou a página do Calcula aí! Deseja prosseguir?')
+function calcular() {
+    var valor_litro = document.getElementById('valor-litro')
+    var km_litro = document.getElementById('km-litro')
+    var distancia = document.getElementById('km')
+    var res = document.getElementById('res')
+    var res1 = Number(valor_litro.value)
+    var res2 = Number(km_litro.value)
+    var res3 = Number(distancia.value)
+    var calculo_final = (res3 / res2) * res1
+    res.innerHTML = `O valor necessário a ser gasto com combustível é de ${calculo_final}.`
+}
